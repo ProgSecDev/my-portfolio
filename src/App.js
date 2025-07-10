0acca5234a43389a95bc25b4bd79d478de92822a
elie <elie_eph@hotmail.com>
2025-07-10 10:08:26 +0300

    Updated 10 July 2025 10:08 AM

--- src/App.js
+++ src/App.js
@@ -34,9 +34,9 @@ function App() {
     if (hash) {
       const el = document.querySelector(hash);
       if (el) {
-        const yOffset = -80;
-        const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
-        window.scrollTo({ top: y, behavior: "smooth" });
+        const yOffset = 80; // adjust to your navbar height
+const y = el.getBoundingClientRect().top + window.pageYOffset - yOffset;
+window.scrollTo({ top: y, behavior: "smooth" });
       }
     }
   }
